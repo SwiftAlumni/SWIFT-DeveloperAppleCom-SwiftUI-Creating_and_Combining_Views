8babf08ef4edcaeb617a3176b6408959455f2a8c
ignasiperez <ignasiperez@gmail.com>
2022-09-13 12:58:55 +0200

    Task1. Change the greeting

---
+++ SW0201P01-C1_1-Landmarks/SW0201P01-C1_1-T01-Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Ignasi Perez-Valls on 12/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+//  Text("Hello, world!")
+    Text("Hello, Ignasi!")
+      .padding()
+  } // body
+  
+} // ContentView
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  } // previews
+  
+} // ContentView_Previews
+
+
+
+
+
+
+
