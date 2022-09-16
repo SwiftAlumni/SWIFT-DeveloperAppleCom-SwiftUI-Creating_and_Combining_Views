782b0ababf86b58cd34bcbb3b84064c4403325a6
ignasiperez <ignasiperez@gmail.com>
2022-09-16 11:55:56 +0200

    Task2. Customize the text view

---
+++ SW0201P01-C1_1-Landmarks/SW0201P01-C1_1-T02-Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Ignasi Perez-Valls on 12/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Turtle Rock")
+      .font(.title)
+  } // body
+  
+} // ContentView
+
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  } // previews
+  
+} // ContentView_Previews
+
+
+
+
+
+
+
---
+++ SW0201P01-C1_1-Landmarks/SW0201P01-C1_1-T02-Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ignasi Perez-Valls on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  } // body
+  
+} // LandmarksApp
