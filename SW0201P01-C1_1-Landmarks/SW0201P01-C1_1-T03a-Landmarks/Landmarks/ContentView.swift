d1f14f95adc965fc8184df13431e5550f3148b0e
ignasiperez <ignasiperez@gmail.com>
2022-09-18 14:47:15 +0200

    Task3a. Add Vertical Stack, Text view and Font

---
+++ SW0201P01-C1_1-Landmarks/SW0201P01-C1_1-T03a-Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Ignasi Perez-Valls on 12/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack(alignment: .leading) {
+      Text("Turtle Rock")
+        .font(.title)
+      Text("Joshua Tree National Park")
+        .font(.subheadline)
+    } // VStack
+  } // body
+  
+} // ContentView
+
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  } // previews
+  
+} // ContentView_Previews
+
+
+
+
+
+
+
