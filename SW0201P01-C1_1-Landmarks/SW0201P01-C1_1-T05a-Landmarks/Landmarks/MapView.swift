89a7f076b27eed7c00963dac4f91c2b37ac6ff05
ignasiperez <ignasiperez@gmail.com>
2022-09-24 19:55:50 +0200

    Task5a. Create a custom view to manage a map

---
+++ SW0201P01-C1_1-Landmarks/SW0201P01-C1_1-T05a-Landmarks/Landmarks/MapView.swift
@@ -0,0 +1,23 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Ignasi Perez-Valls on 24/9/22.
+//
+
+import SwiftUI
+import MapKit
+
+
+struct MapView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+} // MapView
+
+
+struct MapView_Previews: PreviewProvider {
+  static var previews: some View {
+    MapView()
+  }
+}
