94f3eb0ffc42c088a951739e894cbeb086828aa9
ignasiperez <ignasiperez@gmail.com>
2022-09-24 18:50:56 +0200

    Task4b. Create a shadow and an overlay which content is a Circle

---
+++ SW0201P01-C1_1-Landmarks/SW0201P01-C1_1-T04b-Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,28 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Ignasi Perez-Valls on 20/9/22.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+  var body: some View {
+    Image("turtlerock")
+      .clipShape(Circle())
+      .overlay {
+//      Circle().stroke(.gray, lineWidth: 4)
+        Circle().stroke(.white, lineWidth: 4)
+      }
+      .shadow(radius: 7)
+  }
+  
+} // CircleImage
+
+
+struct CircleImage_Previews: PreviewProvider {
+  static var previews: some View {
+    CircleImage()
+  }
+}
---
+++ SW0201P01-C1_1-Landmarks/SW0201P01-C1_1-T04b-Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,19 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ignasi Perez-Valls on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+  var body: some Scene {
+    WindowGroup {
+//    ContentView()
+      CircleImage()
+    }
+  } // body
+  
+} // LandmarksApp
