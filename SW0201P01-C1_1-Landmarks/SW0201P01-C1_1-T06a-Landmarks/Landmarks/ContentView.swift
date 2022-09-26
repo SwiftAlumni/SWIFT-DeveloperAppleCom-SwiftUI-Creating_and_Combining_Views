43ef9e7abe3d2be6bebc7e7249cfd8405b7c76bb
ignasiperez <ignasiperez@gmail.com>
2022-09-26 12:39:09 +0200

    Task6a. Compose the Detail View – Add custom views

---
+++ SW0201P01-C1_1-Landmarks/SW0201P01-C1_1-T06a-Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Ignasi Perez-Valls on 12/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      MapView().frame(height: 300)
+      
+      CircleImage()
+      
+      VStack(alignment: .leading) {
+        Text("Turtle Rock")
+          .font(.title)
+        HStack {
+          Text("Joshua Tree National Park")
+            .font(.subheadline)
+          Spacer()
+          Text("California")
+            .font(.subheadline)
+        }
+      } // VStack
+      .padding()
+    } // Root VStack
+  } // body
+  
+} // ContentView
+
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  } // previews
+  
+} // ContentView_Previews
+
+
+
+
+
+
+
