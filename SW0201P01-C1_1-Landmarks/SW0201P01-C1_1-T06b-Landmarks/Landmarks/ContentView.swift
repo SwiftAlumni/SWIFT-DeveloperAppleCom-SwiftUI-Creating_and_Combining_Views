7bf5070c43d3aa2f0dd6e570c0cb40cabcb4d54b
ignasiperez <ignasiperez@gmail.com>
2022-09-27 11:05:21 +0200

    Task6b. Compose the Detail View – Adjust layout

---
+++ SW0201P01-C1_1-Landmarks/SW0201P01-C1_1-T06b-Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Ignasi Perez-Valls on 12/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      MapView()
+        .frame(height: 300)
+        .ignoresSafeArea(edges: .top)
+      
+      
+      CircleImage()
+        .offset(y: -130)
+        .padding(.bottom, -130)
+
+      VStack(alignment: .leading) {
+        Text("Turtle Rock")
+          .font(.title)
+        HStack {
+          Text("Joshua Tree National Park")
+            .font(.subheadline)
+          Spacer()
+          Text("California")
+            .font(.subheadline)
+        }
+      } // VStack
+      .padding()
+      
+      Spacer()
+    } // Root VStack
+  } // body
+  
+} // ContentView
+
+//        .border(.blue, width: 3)
+
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  } // previews
+  
+} // ContentView_Previews
+
+
+
+
+
+
+
