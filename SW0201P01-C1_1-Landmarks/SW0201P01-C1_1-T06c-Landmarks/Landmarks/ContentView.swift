644185810bd8002cf11711060426853a9d5c02ae
ignasiperez <ignasiperez@gmail.com>
2022-10-03 20:22:51 +0200

    Task6c. Compose the Detail View – Finishing touches

--- SW0201P01-C1_1-Landmarks/SW0201P01-C1_1-T06c-Landmarks/Landmarks/ContentView.swift
+++ SW0201P01-C1_1-Landmarks/SW0201P01-C1_1-T06c-Landmarks/Landmarks/ContentView.swift
@@ -18,16 +18,16 @@ struct ContentView: View {
       CircleImage()
         .offset(y: -130)
         .padding(.bottom, -130)
-
+      
       VStack(alignment: .leading) {
         Text("Turtle Rock")
           .font(.title)
         HStack {
           Text("Joshua Tree National Park")
-//          .font(.subheadline)
+          //          .font(.subheadline)
           Spacer()
           Text("California")
-//          .font(.subheadline)
+          //          .font(.subheadline)
         }
         .font(.subheadline)
         .foregroundColor(.secondary)
