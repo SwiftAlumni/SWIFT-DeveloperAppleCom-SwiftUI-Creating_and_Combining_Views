831f3d4a359c5cb9d16e91a944d42318e72e47fe
ignasiperez <ignasiperez@gmail.com>
2022-10-11 19:57:17 +0200

    Change ContentView to instead display LandmarkList

---
+++ SW0201P01-C1_2-Landmarks/SW0201P01-C1_2-T06a-Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ignasi Perez-Valls on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+  var body: some Scene {
+    WindowGroup {
+//    ContentView()
+//    CircleImage()
+//    MapView()
+//      LandmarkRow(landmark: landmarks[0])
+      LandmarkList()
+    }
+  } // body
+  
+} // LandmarksApp
